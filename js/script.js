9cbea7c5b16bb426a8c3d9af935605823620012a
Olga Kosenko <113343936+Helgako-123@users.noreply.github.com>
2023-01-09 14:00:36 +0100

    Add code to display result of the game

--- js/script.js
+++ js/script.js
@@ -30,3 +30,29 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('You win!');
+  } else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('You lose!');
+  } else if  (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('You lose!');
+  } else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('You win!');
+  } else if  (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('You win!');
+  } else if  (computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('You lose!');
+  } else if  (computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('The game ended in a draw :(');
+  } else if  (computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('The game ended in a draw :(');
+  } else if  (computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('The game ended in a draw :(');
+  } else if (computerMove == 'kamień' && playerMove == 'nieznany ruch') {
+    printMessage('You need to choose 1, 2 or 3');
+  } else if (computerMove == 'papier' && playerMove == 'nieznany ruch') {
+    printMessage('You need to choose 1, 2 or 3');
+  } else if (computerMove == 'nożyce' && playerMove == 'nieznany ruch') {
+    printMessage('You need to choose 1, 2 or 3');
+  } 
